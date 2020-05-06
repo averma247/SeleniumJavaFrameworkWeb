6290ded25e8341cd3a4d87f2f9b4bf8926a89698
Ajay Verma <kash.ajayverma@gmail.com>
2020-05-06 13:03:41 +0530

    added code for failed test scenarios and adding screen shot

--- SeleniumJavaFrameworkWeb/src/test/java/com/listeners/TestNGListeners.java
+++ SeleniumJavaFrameworkWeb/src/test/java/com/listeners/TestNGListeners.java
@@ -1,38 +1,62 @@
 package com.listeners;
 
 import org.apache.logging.log4j.LogManager;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.library.GenericFunctionLib;
+import com.utils.ExtentTestReportManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-public class TestNGListeners implements ITestListener {
+public class TestNGListeners extends GenericFunctionLib implements ITestListener {
 	private static	Logger logger = LogManager.getLogger(TestNGListeners.class);
-	public void onFinish(ITestContext result) {
 
-		logger.info("***** Test Completed *******"+ result.getName());
-		
-	}
+	ExtentReports extent =ExtentTestReportManager.extentRepotGeneratorNG();
+	ExtentTest test;
 
-	public void onStart(ITestContext result) {
+	public void onStart(ITestResult result) {
 		// TODO Auto-generated method stub
+		System.out.println("Inside onStart(ITestContext context)");
 		logger.info("***** Test Started *******"+ result.getName());
+		test = extent.createTest(result.getMethod().getMethodName());
 		
+
 	}
 
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		logger.info("***** Test Failed But with in success percentage *******"+ result.getName());
+		logger.info("***** Test Failed But with in success percentage *******"+ result.getMethod());
 		
 	}
 
 	public void onTestFailure(ITestResult result) {
-		logger.info("***** Test Failed *******"+ result.getName());
+		WebDriver driver=null;
+		logger.info("***** Test Failed *******"+ result.getMethod());
+		test.fail(result.getThrowable());
+		Object testObject=result.getInstance();
+		Class testclass=result.getTestClass().getRealClass();
+		try {
+			driver=(WebDriver)testclass.getDeclaredField("driver").get(testObject);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();		
+		}
+		try {
+			test.addScreenCaptureFromPath(getScreenShotPath(result.getMethod().getMethodName(),driver), result.getMethod().getMethodName());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	public void onTestSkipped(ITestResult result) {
-		logger.info("***** Test Skipped *******"+ result.getName());
-		
+		logger.info("***** Test Skipped *******"+ result.getMethod());
+
 	}
 
 	public void onTestStart(ITestResult result) {
@@ -40,8 +64,24 @@ public class TestNGListeners implements ITestListener {
 	}
 
 	public void onTestSuccess(ITestResult result) {
-		logger.info("***** Test Successfull *******"+ result.getName());
+		logger.info("***** Test Successfull *******"+ result.getMethod().getMethodName());
+		test.log(Status.PASS, "Test is passed successfully ");
 	}
-	
+
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Inside onStartMethod(ITestContext context)");
+		
+	}
+
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Inside onFinish(ITestContext context)");
+		extent.flush();
+		
+	}
+
 
 }
